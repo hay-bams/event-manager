4fef9e88becf8dc9ea50ac9cc9ec4e11121e82b1
purpose50 <purposenigeria@gmail.com>
2018-05-21 00:57:19 +0100

    event http methods

--- app/event/event.py
+++ app/event/event.py
@@ -1,33 +1,116 @@
+from collections import OrderedDict
 from flask import jsonify, request
 from app.event import event_api
-from app.models import Event, User, Center
+from app.models import Event, User, Center, EventSchema
 from app import db
+from app.exception import ValidationError
 
-@event_api.route('/events', methods = ['POST'])
+
+@event_api.route('/events', methods=['POST'])
 def create_event():
-  event = Event()
-  token = request.headers['token']
-  user_id = event.decode_token(token)
-  user = User.query.get(user_id)
-  center = Center.query.get(1)
+    event = Event()
+    token = request.headers['token']
+    user_id = event.decode_token(token)
+    user = User.query.get(user_id)
+    center = Center.query.get(1)
 
-  event.import_data(request.form)
-  event.user = user
-  event.center = center
+    event.import_data(request.form)
+    event.user = user
+    event.center = center
 
-  db.session.add(event)
-  db.session.commit()
+    db.session.add(event)
+    db.session.commit()
 
-  return jsonify({
-    'message': 'Event created successfully'
-  })
+    event_schema = EventSchema()
+    result = event_schema.dump(event).data
 
-@event_api.route('/events/<int:eventId>', methods = ['GET', 'PUT'])
-def edit_event():
-  event= request.get_json()
+    return jsonify({
+        'message': 'Event created successfully',
+        'events': result
+    }), 201
 
-@event_api.route('/events/<int:eventId>', methods = ['DELETE'])
-def delete_event():
-  return ''
+
+@event_api.route('/events', methods=['GET'])
+def get_all_events():
+    all_events = Event.query.all()
+    events_schema = EventSchema(many=True)
+    result = events_schema.dump(all_events)
+
+    return jsonify({
+        'success': 'true',
+        'message': 'message retrieved successfully',
+        'event': result.data 
+    }), 200
+
+
+@event_api.route('/events/<int:eventId>', methods=['GET'])
+def get_one_event(eventId):
+    event = Event.query.get(eventId)
+
+    if not event:
+        return jsonify({
+            'success': 'false',
+            'message': 'Event does not exist'
+        }), 404
+
+    event_schema = EventSchema()
+    result = event_schema.dump(event)
+
+    return jsonify({
+        'success': 'true',
+        'message': 'message retrieved successfully',
+        'event': result.data
+    }), 200
+
+
+@event_api.route('/events/<int:eventId>', methods=['PUT'])
+def edit_event(eventId):
+    try:
+        event = Event.query.get(eventId)
+
+        if not event:
+            return jsonify({
+                'success': 'false',
+                'message': 'Event does not exist'
+            }), 404
+
+        if not request.form['event_name'] or not request.form['event_date']:
+            pass
+
+        event.event_name = request.form['event_name']
+        event.event_date = request.form['event_date']
+
+        db.session.add(event)
+        db.session.commit()
+
+        event_schema = EventSchema()
+        result = event_schema.dump(event)
+        return jsonify({
+            'success': 'true',
+            'message': 'event updated successfully',
+            'event': result.data
+        }), 201
+
+    except KeyError as error:
+        raise ValidationError('Invalid entries, missing ' + error.args[0])
+
+
+@event_api.route('/events/<int:eventId>', methods=['DELETE'])
+def delete_event(eventId):
+    event = Event.query.get(eventId)
+
+    if not event:
+        return jsonify({
+            'success': 'false',
+            'message': 'Event does not exist'
+        }), 404
+
+    db.session.delete(event)
+    db.session.commit()
+
+    return jsonify({
+        'success': 'true',
+        'message': 'Event deleted successfully'
+    }), 200
 
 
